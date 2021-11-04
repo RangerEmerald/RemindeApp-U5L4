5c240e97edb525d9b726bb852b7bad875c64bf6c
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-03 19:13:26 -0600

    Made scrolling reminders work

--- src/main/java/io/github/rangeremerald/RemindeApp/screens/Interface.java
+++ src/main/java/io/github/rangeremerald/RemindeApp/screens/Interface.java
@@ -11,7 +11,7 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Interface extends JPanel implements ActionListener, MouseListener {
+public class Interface extends JPanel implements ActionListener, MouseListener, KeyListener, MouseWheelListener {
 
     public final static ArrayList<Reminds> reminders = new ArrayList<>(); // Stores all the reminders
     public final static HashMap<String, Buttons> buttons = new HashMap<>(); // Stores all the buttons on the main interface
@@ -19,18 +19,21 @@ public class Interface extends JPanel implements ActionListener, MouseListener {
     private Reminds mouseOverRemind = null; // Stores which reminder the mouse is over
 
     // Stores the reminders background so they can be accessed elsewhere for ease of use
-    public final static int remindersWidth = 300;
-    public final static int remindersX = RemindeApp.remindWidth / 2 - remindersWidth / 2;
-    public final static int remindersY = 80;
+    public final static Rectangle reminderBackground = new Rectangle(RemindeApp.remindWidth / 2 - 300 / 2, 80, 300, 300);
 
     private final Timer timer;
 
     public Interface() {
+        for (int i = 0; i < 21; ++i) reminders.add(new Reminds("lsdfasdf" + i, 10));
+
+        addKeyListener(this);
+        addMouseListener(this);
+        addMouseWheelListener(this);
+
         timer = new Timer(5, this);
         timer.start();
 
         initButtons();
-        reminders.add(new Reminds("Do lunch Do lunch Do lunch Do lunch Do lunch Do lunch Do lunch Do lunch Do lunch Do lunch Do lunch ", 1000, reminders.size()));
     }
 
     // Initializes all the buttons the main interface will be using
@@ -50,18 +53,24 @@ public class Interface extends JPanel implements ActionListener, MouseListener {
 
         // Sets the background of the scrollable reminders to white centered horizontally to the app
         g.setColor(Color.white);
-        g.fillRect(remindersX, remindersY, remindersWidth, 300);
+        g.fillRoundRect(reminderBackground.x, reminderBackground.y, reminderBackground.width, reminderBackground.height, 20, 20);
+
+        // Draws all the reminders
+        for (Reminds reminder : reminders) reminder.drawRemind(g);
+
+        // Draws the overflow reminder hiders
+        g.setColor(Color.lightGray);
+        g.fillRect(0, 0, RemindeApp.remindWidth, reminderBackground.y); // Top overflow hider
+        g.fillRect(0, reminderBackground.y + reminderBackground.height, RemindeApp.remindWidth, RemindeApp.remindHeight - (reminderBackground.y + reminderBackground.height));
 
         // Drawing the title of the app centered horizontally to the app
+        g.setColor(Color.white);
         Font titleFont = new Font("san-serif", Font.PLAIN, 40);
         String title = "Reminders";
 
         g.setFont(titleFont);
 
         g.drawString(title, RemindeApp.remindWidth / 2 - g.getFontMetrics(titleFont).stringWidth(title) / 2, 40);
-
-        // Draws all the reminders
-        for (Reminds reminder : reminders) reminder.drawRemind(g);
 
         // Draws all the buttons
         for (Buttons button : buttons.values()) button.drawButton(g);
@@ -86,7 +95,7 @@ public class Interface extends JPanel implements ActionListener, MouseListener {
                 isMouseHovered = true;
             } // Checks if the cursor is over a button
 
-            for (Reminds remind : reminders) if (mouse.intersects(remind.remindRect)) {
+            for (Reminds remind : reminders) if (mouse.intersects(remind.remindRect) && remind.isVisible) {
                 setCursor(new Cursor(Cursor.HAND_CURSOR));
                 isMouseHovered = true;
                 overRemind = true;
@@ -105,6 +114,11 @@ public class Interface extends JPanel implements ActionListener, MouseListener {
     }
 
     @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_N) buttons.get("addRemind").buttonAction();
+    }
+
+    @Override
     public void mouseReleased(MouseEvent e) {
         try {
             Rectangle mouse = new Rectangle(e.getX(), e.getY(), 1, 1); // Makes a custom mouse hitbox so the program can detect if the mouse is over a button or reminder
@@ -114,6 +128,19 @@ public class Interface extends JPanel implements ActionListener, MouseListener {
             for (Reminds reminds : reminders) if (mouse.intersects(reminds.remindRect)) reminds.remindAction();
         } catch (IllegalComponentStateException exception) { // Catches and prints exception so the app does not crash
             System.out.println(exception);
+        }
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        // Checks to make sure the reminders aren't just scrolling into infinity, and are constrained
+        if (reminders.size() > 0) {
+            int remindDir = 0;
+            int botMostReminder = reminders.size() - 1;
+            if (e.getWheelRotation() > 0 && reminders.get(botMostReminder).remindRect.y +  reminders.get(botMostReminder).remindRect.height >= reminderBackground.y + reminderBackground.height) remindDir = -5; // Checks if the bottom most reminder is below the background
+            else if (e.getWheelRotation() < 0 && reminders.get(0).remindRect.y <= reminderBackground.y) remindDir = 5; // Checks if the top most reminder is above the top of the background
+
+            if (remindDir != 0) for (Reminds reminds : reminders) reminds.moveRemind(remindDir);
         }
     }
 
@@ -135,6 +162,16 @@ public class Interface extends JPanel implements ActionListener, MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
